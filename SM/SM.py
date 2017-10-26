b24d9841f8b7ae4b856db795bca35376438978a2
Rob Maas <robmaas77@hotmail.com>
2017-10-26 08:29:14 +0200

    Restructured folder and everything should work now

--- SM/SM.py
+++ SM/SM.py
@@ -4,7 +4,7 @@ Script to download the required Sentinel-2 data, longer description later.
 
 
 import pandas as pd
-df = pd.read_csv('Test.csv', sep=',')
+df = pd.read_csv('.\Data\Validation\Raam\data\station_data\RM_SM_01_calibrated.csv', sep=',')
 
-for i in range(29):
-    print(df)
+print(df)
+
